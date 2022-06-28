bc40a5ed9d574528971a77208268271e29b3c5e7
andrew <julieyeng.js@gmail.com>
2022-06-28 20:26:18 +0700

    ok can push alrd

--- app.js
+++ app.js
@@ -45,9 +45,10 @@ function Stacker(id, routine, warm1, warm2, try1, try2, try3, best) {
 //------------------------------------------------------------------------------------------------------
 
 
+
 io.on('connection', (sock) => {
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LISTEN FROM CLIENT - CONNECTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    sock.on('newuser', (data) => {
+    /* sock.on('newuser', (data) => {
         
         sock.id = data;
         var sysmessage = sock.id + " connected";
@@ -69,107 +70,25 @@ io.on('connection', (sock) => {
         }
         io.emit('sendconnected', connectedUsers);
 
-    });
+    }); */
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LISTEN FROM CLIENT - CONNECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LISTEN FROM CLIENT - ADD & MINUS WINS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LISTEN FROM CLIENT - ADD & MINUS CHALLENGES ~~~~~~~~~~~~~~~~~~~~~~~~~~
-    sock.on('submit', (data) => {
-
-        var index = users.indexOf(data.nickname);
-        objUsers[index].results[roundNum - 1] = data.result;
-        objUsers[index].penalties[roundNum - 1] = data.penalties;
-        console.log("The result received on server is " + objUsers[index].results[roundNum - 1] + " for user " + objUsers[index].id);
-        io.emit('reshistory', objUsers[index]);
-
-    });
-
-    sock.on('editresult', (data) => {
-
-        var index = users.indexOf(data.student);
-        objUsers[index].results[data.round - 1] = data.result;
-        objUsers[index].penalties[data.round - 1] = data.penalties;
-
-    });
-
+    
     sock.on('chat-to-server', (data) => {
         io.emit('chat-to-clients', data);
     });
 
-    sock.on('challengethisuser', (data) => {
-        io.emit('appendchallenger', data);
+    sock.on('gameStart', () => {
+        io.emit('startGame');
     });
 
-    sock.on('challenge', (data) => {
-        var userId = data;
-        io.emit('sendchallenge', userId);
-
-    });
-
-    sock.on('nextround', () => {
-        roundNum++;
-        io.emit('refreshall', roundNum);
-    });
 
 //############################################################################################################
 
-    sock.on('updatename', (data) => {
-        io.emit('clientupdatename', data);
-        for (var i = 1; i <= 29; i++) {
-            if (objects[i].player === data.id) {
-                objects[i].pcontent = data.plyr;
-                console.log("Record saved: " + objects[i].pcontent + " at record: " + objects[i].player);
-            }
-        }
-        
-    });
-
-    sock.on('updatename2', (data) => {
-        io.emit('clientupdatename2', data);
-        for (var i = 1; i <= 29; i++) {
-            if (objects[i].player === data.id2) {
-                objects[i].pcontent = data.plyr;
-                console.log("Record saved: " + objects[i].pcontent + " at record: " + objects[i].player);
-            }
-        }
-    });
-
-    sock.on('updateresult', (data) => {
-        io.emit('clientupdateresult', data);
-        for (var i = 1; i <= 29; i++) {
-            if (objects[i].result === data.id) {
-                objects[i].rcontent = data.notInt;
-                console.log("Record saved: " + objects[i].rcontent + " at record: " + objects[i].result);
-            }
-        }
-    });
-
-    sock.on('updatetime', (data) => {
-        io.emit('clientupdatetime', data);
-        for (var i = 1; i <= 29; i++) {
-            if (objects[i].time === data.input) {
-                objects[i].tcontent = data.keyin;
-                console.log("Record saved: " + objects[i].tcontent + " at record: " + objects[i].time);
-            }
-        }
-
-    });
-
-    sock.on('updatewincon', (data) => {
-        io.emit('clientupdatewincon', data);
-        winCon = data;
-    });
-
-    sock.on('resetserverval', () => {
-        console.log("Server reseted")
-        objects = {};
-        connectedUsers = [];
-        for (var i = 1; i <= 28; i++) {
-            objects[i] = new Record(pIdArr[i-1], "--", tIdArr[i-1], "", rIdArr[i-1], 0);
-        }
-    });
 
     sock.on('updatehighlight', (data) => {
         var eleId = data;
@@ -179,129 +98,22 @@ io.on('connection', (sock) => {
         
     });
 
-    sock.on('updatehighlight2', (data) => {
-        var eleId = data;
-        highlightedBox = !highlightedBox;
-
-        io.emit('highlight2', {eleId, highlightedBox});
-        
+    sock.on('mouseMove', (data) => {
+        //console.log("X is " + data.x + " and Y is " + data.y);
+        var x = data.x;
+        var y = data.y;
+        var player = data.player;
+        io.emit('newPosition', { x, y, player });
     });
 
-    sock.on('reupdateobjects', () => {
-        io.emit("updateonconnect", {objects});
-        console.log("reupdate objects runned");
-    });
+    
 
-    sock.on('updStacker', (data) => {
-        allStackers.forEach((stacker) => {
-            if (stacker.id === data.stacker) {
-                dProperty.forEach((property) => {
-                    if (data.whichTry === property) {
-                        stacker[property] = parseFloat(data.result);
-                        /* console.log(stacker.try1);
-                        console.log(property); */
-                    }
-                    if (stacker.best === 0 && property != "warm1" && property != "warm2") {
-                        stacker.best = stacker[property];
-                    } else {
-                        if (stacker.best > stacker[property] && stacker[property] != 0 && property != "warm1" && property != "warm2") {
-                            stacker.best = stacker[property];
-                        } else {};
-                    }
-
-                });
-
-                if (stacker.try1 != 0 && stacker.try2 != 0 && stacker.try3 != 0) {
-                    let arr = [];
-                    arr.push(stacker.try1);
-                    arr.push(stacker.try2);
-                    arr.push(stacker.try3);
-
-                    arr.sort(function(a,b) {
-                        return a - b;
-                    });
-
-                    stacker.best = arr[0];
-                    
-                }
-            }
-        });
-
-        if (stacker11.id === data.stacker) {
-            dProperty.forEach((property) => {
-                if (data.whichTry === property) {
-                    stacker11[property] = parseFloat(data.result);
-                    /* console.log(stacker.try1);
-                    console.log(property); */
-                }
-                if (stacker11.best === 0 && property != "warm1" && property != "warm2") {
-                    stacker11.best = stacker11[property];
-                } else {
-                    if (stacker11.best > stacker11[property] && stacker11[property] != 0 && property != "warm1" && property != "warm2") {
-                        stacker11.best = stacker11[property];
-                    } else {};
-                }
-
-            });
-
-            if (stacker11.try1 != 0 && stacker11.try2 != 0 && stacker11.try3 != 0) {
-                let arr = [];
-                arr.push(stacker11.try1);
-                arr.push(stacker11.try2);
-                arr.push(stacker11.try3);
-
-                arr.sort(function(a,b) {
-                    return a - b;
-                });
-
-                stacker11.best = arr[0];
-                
-            }
-        }
-
-        
-        allStackers.sort(function(a,b) {
-
-            if (a.best === 0) return 1;        
-            if (b.best === 0) return -1; 
-            return a.best - b.best;
-            
-        });
-    });
-
-    sock.on('manualAssign', (data) => {
-        var stacker = data.student2;
-        var judge = data.student;
-        io.emit('findJudge', { judge, stacker});
-    });
-
-    sock.on('changeEvent', (data) => {
-        theEvent = data;
-        io.emit('chgEventClients', theEvent);
-    });
-
-    sock.on('resetResults', () => {
-        allStackers.forEach((stacker) => {
-            stacker.try1 = 0;
-            stacker.try2 = 0;
-            stacker.try3 = 0;
-            stacker.best = 0;
-        })
-
-        stacker11.try1 = 0;
-        stacker11.try2 = 0;
-        stacker11.try3 = 0;
-        stacker11.best = 0;
-    });
-
-    sock.on('checkStu', (data) => {
-        var checkStu = data;
-        io.emit('checkStuClients', checkStu);
-    });
+    
 
 
 
 });
+
 
 
 
--- client/js/index.js
+++ client/js/index.js
@@ -2,7 +2,7 @@ const sock = io();
 
 
 //---------------------------------------- USER PIN NUMBER PROMPT -----------------------------------------
-/* const promptMsg = () => {
+const promptMsg = () => {
     var nick = prompt("Please enter your pin number:");
     while (nick.length == 0) {
         alert("Please enter your pin number!");
@@ -77,17 +77,214 @@ const sock = io();
         alert("Wrong pin number!");
         promptMsg();
     }
-}; */
+};
 
-/* promptMsg();
-sock.emit('newuser', nickname); */
+promptMsg();
+sock.emit('newuser', nickname);
 //---------------------------------------- USER PIN NUMBER PROMPT -----------------------------------------
 
+var playerOne = "TCR";
+var playerTwo = "LOK";
+
+const cursor = document.querySelector('.cursor');
+const cursor2 = document.querySelector('.cursor2');
+const holes = [...document.querySelectorAll('.hole')];
+const holeArr = [8, 2, 0, 4, 3, 5, 8, 7, 1, 6, 2, 7];
+var i = 0;
+const scoreEl = document.querySelector('.score span');
+let score = 0;
+
+function createChatDivs() {
+    var chatDiv = document.createElement("div");
+//var chatDiv = document.getElementById("chatdiv");
+    //chatDiv.setAttribute("id", "chatdiv");
+    chatDiv.style.width = "450px";
+    chatDiv.style.height = "339px";
+    //chatDiv.style = "background:rgba(255, 255, 255, 0.5); color:black; overflow: auto;"
+    chatDiv.style.background = "rgba(255, 255, 255, 0.5)";
+    chatDiv.style.color = "black";
+    chatDiv.style.overflow = "auto";
+    chatDiv.style.overflowX = "hidden";
+    chatDiv.style.float = "right";
+    chatDiv.style.marginLeft = "2%";
+    chatDiv.style.position = "fixed";
+    chatDiv.style.top = "30px";
+    chatDiv.style.right = "30px";
 
 
+document.body.append(chatDiv);
 
+var chatInput = document.createElement('input');
+chatInput.className = "form-control";
+chatInput.style.width = "338px";
+chatInput.style.height = "48px";
+chatInput.setAttribute("id", "chatinput");
+chatInput.setAttribute("type", "text");
+chatInput.style.display = "inline";
+chatDiv.appendChild(chatInput);
 
+var chatBtn = document.createElement('button');
 
+chatBtn.className = "btn btn-secondary";
+chatBtn.setAttribute("id", "chatBtn");
+chatBtn.innerHTML = "Send";
+
+chatDiv.appendChild(chatBtn);
+
+var div3 = document.createElement('div');
+div3.setAttribute("id", "div3");
+div3.style.width = '420px';
+div3.style.height = '280px'
+div3.style.color = 'black';
+div3.style.background = 'rgba(236, 236, 236, 0.5)';
+div3.style.overflowY = "auto";
+chatDiv.appendChild(div3);
+
+chatBtn.addEventListener('click', function () {
+    var message = nickname + ': ';
+    message += chatInput.value;
+    sock.emit('chat-to-server', message);
+    chatInput.value = '';
+});
+
+chatInput.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("chatBtn").click();
+    }
+
+});
+
+return chatDiv;
+}
+
+function appendMessage(message) {
+    const messageDiv = document.createElement('div');
+    messageDiv.innerText = message;
+    var div3 = document.getElementById("div3");
+    div3.append(messageDiv);
+    div3.scrollTop = div3.scrollHeight;
+    if (message === "TCR: RESET SERVER") {
+        if (nickname != "TCR") {
+            window.location.reload();
+        } else {
+            sock.emit('resetserverval');
+        }
+    }
+
+    //SET P1 - P4
+    
+
+    if (message === "TCR: Game start" && nickname === "TCR") {
+        sock.emit('gameStart');
+    }
+    
+
+    if (message === "TCR: NUMBER OF PLAYERS" && nickname === "TCR") {
+        //let text = "[" + connectedArr.toString() + "]";
+        sock.emit('chat-to-server', numberOfPlayers);
+
+    }
+
+}
+
+function mouseDragged(mouseX, mouseY, player) {
+
+    var data = {
+        x: mouseX,
+        y: mouseY,
+        player: player
+    }
+    sock.emit('mouseMove', data);
+}
+
+function run() {
+    //const i = Math.floor(Math.random() * holes.length);
+    const hole = holes[holeArr[i]];
+    let timer = null;
+
+    const img = document.createElement('img');
+    const img2 = document.createElement('img');
+    img.classList.add('mole');
+    img2.classList.add('mole2');
+    img.src = 'https://raw.githubusercontent.com/0shuvo0/whack-a-mole/main/assets/mole.png';
+    img2.src = 'https://raw.githubusercontent.com/0shuvo0/whack-a-mole/main/assets/mole-whacked.png';
+    img.addEventListener('click', () => {
+        if (nickname === playerOne) {
+            score += 10;
+            scoreEl.textContent = score;
+            //img.src = 'https://raw.githubusercontent.com/0shuvo0/whack-a-mole/main/assets/mole-whacked.png';
+            hole.removeChild(img);
+            hole.appendChild(img2);
+            clearTimeout(timer);
+            setTimeout(() => {
+                hole.removeChild(img2);
+                i++;
+                run();
+            }, 500);
+        }
+        if (nickname === playerTwo) {
+            score += 10;
+            scoreEl.textContent = score;
+            //img.src = 'https://raw.githubusercontent.com/0shuvo0/whack-a-mole/main/assets/mole-whacked.png';
+            hole.removeChild(img);
+            hole.appendChild(img2);
+            clearTimeout(timer);
+            setTimeout(() => {
+                hole.removeChild(img2);
+                i++;
+                run();
+            }, 500);
+        }
+
+    });
+    hole.appendChild(img);
+
+    timer = setTimeout(() => {
+        hole.removeChild(img);
+        i++;
+        run();
+    }, 1500);
+
+    sock.on('newPosition', (data) => {
+        //console.log("X is " + data.x + ", Y is " + data.y);
+        if (data.player === "P1") {
+            cursor.style.top = data.y;
+            cursor.style.left = data.x;
+        }
+        if (data.player === "P2") {
+            cursor2.style.top = data.y;
+            cursor2.style.left = data.x;
+        }
+    });
+}
+
+createChatDivs();
+
+//run();
+
+window.addEventListener('mousemove', e => {
+    if (nickname === playerOne) {
+        //cursor.style.top = e.pageY + 'px';
+        var pMouseY = e.pageY + 'px';
+        //cursor.style.left = e.pageX + 'px';
+        var pMouseX = e.pageX + 'px';
+        mouseDragged(pMouseX, pMouseY, "P1");
+    }
+    if (nickname === playerTwo) {
+        var pMouseY = e.pageY + 'px';
+        var pMouseX = e.pageX + 'px';
+        mouseDragged(pMouseX, pMouseY, "P2");
+    }
+
+});
+
+window.addEventListener('mousedown', () => {
+    cursor2.classList.add('active');
+});
+window.addEventListener('mouseup', () => {
+    cursor2.classList.remove('active');
+});
 
 
 
@@ -100,7 +297,13 @@ sock.emit('newuser', nickname); */
         sock.emit('chat-to-server', text);
     }
 }); */
+sock.on('chat-to-clients', data => {
+    appendMessage(data);
+});
 
+sock.on('startGame', () => {
+    run();
+});
 
 
 
